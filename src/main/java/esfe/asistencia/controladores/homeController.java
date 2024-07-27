a2532e88b540613962c6495c4cb05a0c008c8172
davidwinxy <daviddiaz04d@gmail.com>
2024-07-26 21:11:53 -0600

    commit uno

---
+++ src/main/java/esfe/asistencia/controladores/homeController.java
@@ -0,0 +1,12 @@
+package esfe.asistencia.controladores;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class homeController {
+    @RequestMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
